35662c9661bcef8ac6c8b651ccc5c6ffe59592d5
Dmitriy <disolokhin@gmail.com>
2026-06-05 10:07:59 -0500

    remove observer on catalyst instance destruction

--- android/src/main/java/com/darryncampbell/rndatawedgeintents/RNDataWedgeIntentsModule.java
+++ android/src/main/java/com/darryncampbell/rndatawedgeintents/RNDataWedgeIntentsModule.java
@@ -135,6 +135,7 @@ public class RNDataWedgeIntentsModule extends ReactContextBaseJavaModule impleme
 
     @Override
     public void onCatalystInstanceDestroy() {
+        ObservableObject.getInstance().deleteObserver(this);
         unregisterReceivers();
     }
 
